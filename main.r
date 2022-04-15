75c57591b6ea9adcf471e961d463925e05612fd5
Richa Purba <richathorne@gmail.com>
2022-04-15 17:40:51 +0000

    Vektor

--- main.r
+++ main.r
@@ -1,2 +1,19 @@
 # R adalah bahasa yang banyak dikuasai dan menjadi pilihan banyak Data Scientist dunia karena penggunaannya yang cukup mudah di bandingkan bahasa lain seperti Java. Pemula sangat disarankan untuk memulai dengan R untuk mempelajari basic dari Data Science itu sendiri.
 # Jika ingin coding dengan R, baik pemula maupun profesional, sangat disarankan menggunakan R Studio dengan UI yang ramah bagi user.
+
+# VEKTOR
+# Vektor adalah variable yang dapat memuat lebih dari satu data (mirip array). Syntax nya adalah sebagai berikut:
+# Elemen -> c(1,2,3,4,5)				--> syntax paling dasar
+# Rentang -> 2:7						--> mengurutkan angka dari 2 sampai 7
+# Urutan -> seq(from=1, to=7, by=1)		--> mengurutkan angka dari 1 sampai 7 dengan selisih 1
+#			seq(1,7,1)					--> shortcut untuk vektor urutan
+# Ulang -> rep(1, times=10)				--> mengulangi angka 1 sampai 10 kali
+#		   rep(1,10)					--> shortcut untuk vektor pengulangan
+
+# Vektor dapat menggunakan operasi aritmetika jika jumlah elemen antara 2 vektor sama banyak.
+vektor1 = c(2,4,6,8) * 3				# setiap elemen vektor dikali dengan 3 sehingga hasilnya (6,12,18,24)
+vektor2 = c(1,2,3,4)
+vektor1 + vektor2						# setiap elemen vektor1 akan dijumlahkan tunggal dengan elemen vektor2 sehingga hasilnya (3,6,9,12)
+vektor3 = c(6,7)
+vektor1 - vektor3						# akan ERROR dikarenakan jumlah elemen vektor1 tidak sama dengan jumlah elemen vektor3
+vektor4 = c("Data", "Big", 9)			# angka 9 akan dianggap sebagai string seperti elemen lainnya sehingga jika dioperasikan dengan aritmatika akan menghasilkan ERROR
