ed29e32f58175005ebe2c33ed28a1222c605752e
Richa Purba <richathorne@gmail.com>
2022-04-15 17:22:15 +0000

    Intro

--- main.r
+++ main.r
@@ -1,1 +1,2 @@
-print("hello world")
+#R adalah bahasa yang banyak dikuasai dan menjadi pilihan banyak Data Scientist dunia karena penggunaannya yang cukup mudah di bandingkan bahasa lain seperti Java. Pemula sangat disarankan untuk memulai dengan R untuk mempelajari basic dari Data Science itu sendiri.
+
