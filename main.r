97736b36b2cecd4c2a4276b1fa285db50dcfe40d
Richa Purba <richathorne@gmail.com>
2022-04-17 10:54:06 +0000

    Matriks

--- main.r
+++ main.r
@@ -17,3 +17,26 @@ vektor1 + vektor2						# setiap elemen vektor1 akan dijumlahkan tunggal dengan e
 vektor3 = c(6,7)
 vektor1 - vektor3						# akan ERROR dikarenakan jumlah elemen vektor1 tidak sama dengan jumlah elemen vektor3
 vektor4 = c("Data", "Big", 9)			# angka 9 akan dianggap sebagai string seperti elemen lainnya sehingga jika dioperasikan dengan aritmatika akan menghasilkan ERROR
+
+# MATRIKS
+# Matriks adalah angka yang disusun menjadi baris dan kolom. Syntax nya adalah sebagai berikut.
+
+mat = matrix(c(1,2,3,4,5,6,7,8,9), nrow=3, byrow=TRUE)
+# Hasil nya akan membentuk matriks ordo 3x3 seperti berikut.
+# 		(,1)	(,2)	(,3)
+#(1,)	 1		 2		 3
+#(2,)	 4		 5		 6
+#(1,)	 7		 8		 9
+# nrow merupakan attribute yang menentukan jumlah baris yang kita inginkan. Apabila elemen matriks tidak penuh sesuai ordo, maka akan terjadi ERROR.
+# byrow merupakan attribute yang menentukan posisi urutan elemen matriks. Jika value TRUE, maka elemen matriks akan disusun dari kiri ke kanan sampai penuh. Jika FALSE, maka elemen akan disusun dari atas ke bawah.
+
+# Pengambilan data matriks dapat dilakukan seperti berikut.
+matA = mat(1,3)					# Mengambil elemen di baris pertama, kolom ketiga yaitu 3
+matB = mat((1:2),3)				# Mengambil elemen di baris pertama dan kedua di kolom ketiga, yaitu 3 dan 6
+
+# Matriks dapat digunakan dengan operasi aritmatika.
+mat * 3							# Setiap elemen dalam matriks akan dikalikan dengan 3
+
+mat1 = matrix(c(2,3,4,5,6,7,8,9,10), nrow=3, byrow=FALSE)
+mat * mat1
+# Setiap elemen dalam kedua matriks akan dikalikan masing-masing sesuai posisinya. Apabila kedua matriks tidak sama jumlah elemennya, maka akan terjadi ERROR.
